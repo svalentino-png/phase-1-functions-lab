991ced34a06b54cfe7e11d8ab199b7ce25189f5b
Sam Valentino <sammyvalentino7@gmail.com>
2025-01-18 14:42:00 -0500

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const HQ = 42;
+
+function distanceFromHqInBlocks(start) {
+    return Math.abs(start - HQ);
+} 
+
+function distanceFromHqInFeet(start) {
+    return distanceFromHqInBlocks(start) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
+
